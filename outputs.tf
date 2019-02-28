41073c5605f0d6278e78fadbacc527dfeaa4c25a
Aleksey Zhukov <alex@izhukov.ru>
2019-02-28 17:31:07 +0300

    Add output of the ARN of the worker nodes IAM role

--- outputs.tf
+++ outputs.tf
@@ -68,6 +68,11 @@ output "security_group_name" {
   value       = "${join("", aws_security_group.default.*.name)}"
 }
 
+output "worker_role_arn" {
+  description = "ARN of the worker nodes IAM role"
+  value       = "${aws_iam_role.default.arn}"
+}
+
 output "config_map_aws_auth" {
   description = "Kubernetes ConfigMap configuration for worker nodes to join the EKS cluster. https://www.terraform.io/docs/providers/aws/guides/eks-getting-started.html#required-kubernetes-configuration-to-join-worker-nodes"
   value       = "${join("", data.template_file.config_map_aws_auth.*.rendered)}"
