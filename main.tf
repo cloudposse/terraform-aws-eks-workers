63aac616807327b5b6d75c3505210fe7fdde2c11
Dmytro Lukianchikov <d.lukyanchikov@anchorfree.com>
2019-05-02 17:39:37 -0700

    fixes

--- main.tf
+++ main.tf
@@ -214,7 +214,7 @@ data "template_file" "userdata" {
 }
 
 data "aws_iam_instance_profile" "default" {
-  count = "${var.enabled == "true" && var.use_provided_aws_iam_instance_profile == "false" ? 1 : 0}"
+  count = "${var.enabled == "true" && var.use_provided_aws_iam_instance_profile == "true" ? 1 : 0}"
   name  = "${var.aws_iam_instance_profile}"
 }
 
@@ -223,6 +223,6 @@ data "template_file" "config_map_aws_auth" {
   template = "${file("${path.module}/config_map_aws_auth.tpl")}"
 
   vars {
-    aws_iam_role_arn = "${var.use_provided_aws_iam_instance_profile == "false" ? join("", aws_iam_role.default.*.arn) : data.aws_iam_instance_profile.default.role_arn}"
+    aws_iam_role_arn = "${var.use_provided_aws_iam_instance_profile == "true" ? data.aws_iam_instance_profile.default.role_arn : join("", aws_iam_role.default.*.arn)}"
   }
 }
