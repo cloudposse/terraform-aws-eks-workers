c836e4c84c9a7b5987db309858021d8ce92bacaf
Tony Thayer-Osborne <tony.thayer@skyward.io>
2020-04-24 09:04:02 -0700

    Fixing spaces around equals

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ locals {
 }
 
 module "label" {
-  source      = "git::https://github.com/cloudposse/terraform-null-label.git?ref = tags/0.16.0"
+  source      = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.16.0"
   namespace   = var.namespace
   stage       = var.stage
   environment = var.environment
