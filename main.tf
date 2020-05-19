ee16fd3571ac378389e9d759e159e8a3f7d1f266
Tony Thayer-Osborne <tony.thayer@skyward.io>
2020-05-19 10:44:09 -0700

    Updated autoscale module allows the use of environment value

--- main.tf
+++ main.tf
@@ -171,7 +171,7 @@ data "aws_iam_instance_profile" "default" {
 }
 
 module "autoscale_group" {
-  source = "git::https://github.com/cloudposse/terraform-aws-ec2-autoscale-group.git?ref=tags/0.4.0"
+  source = "git::https://github.com/cloudposse/terraform-aws-ec2-autoscale-group.git?ref=tags/0.5.0"
 
   enabled     = var.enabled
   namespace   = var.namespace
