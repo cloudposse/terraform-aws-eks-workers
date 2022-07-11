212e900f7295f4fcae9f07d4d306b1c5efb191fc
Nuru <Nuru@users.noreply.github.com>
2022-07-11 13:58:27 -0700

    Revert "Add option to use permissions boundary on the default roles (#85)"
    This reverts commit ea1c1bc6782f2e25d0fbe14aceba13aec08edfbd.

--- main.tf
+++ main.tf
@@ -34,11 +34,10 @@ data "aws_iam_policy_document" "assume_role" {
 }
 
 resource "aws_iam_role" "default" {
-  count                = local.enabled && var.use_existing_aws_iam_instance_profile == false ? 1 : 0
-  name                 = module.label.id
-  assume_role_policy   = join("", data.aws_iam_policy_document.assume_role.*.json)
-  tags                 = module.label.tags
-  permissions_boundary = var.permissions_boundary
+  count              = local.enabled && var.use_existing_aws_iam_instance_profile == false ? 1 : 0
+  name               = module.label.id
+  assume_role_policy = join("", data.aws_iam_policy_document.assume_role.*.json)
+  tags               = module.label.tags
 }
 
 resource "aws_iam_role_policy_attachment" "amazon_eks_worker_node_policy" {
